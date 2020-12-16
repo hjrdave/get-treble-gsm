6b280791668848dbedd5e37c6c71737f946d2b79
David Sanders <dsanders_m633@outlook.com>
2020-12-16 07:54:21 -0500

    added search components

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,28 +1,28 @@
 import React, { useState, useEffect } from "react";
-import PageContainer from '../../components/page-container';
-import PageContent from '../../components/page-content';
-import SearchForm from '../../components/search-form';
-import SearchResults from '../../components/search-results';
+import SEO from '../../components/seo';
+import PageContainer from "../../components/page-container";
+import PageContent from "../../components/page-content";
+import SearchForm from './search-form';
+import SearchResults from './search-results';
 
-
-const SearchPage = ({ location }) => {
-
-  const [results, setResults] = useState([]);
-  const searchQuery = new URLSearchParams(location.search).get('keywords') || '';
+export default function Page({ location }) {
+  const [results, setResults] = useState([])
+  const searchQuery = new URLSearchParams(location.search).get("keywords") || ""
 
   useEffect(() => {
     if (window.__LUNR__) {
       window.__LUNR__.__loaded.then(lunr => {
-        const refs = lunr.en.index.search(searchQuery);
-        const posts = refs.map(({ ref }) => lunr.en.store[ref]);
-        setResults(posts);
-      });
+        const refs = lunr.en.index.search(searchQuery)
+        const posts = refs.map(({ ref }) => lunr.en.store[ref])
+        setResults(posts)
+      })
     }
-  }, [location.search]);
+  }, [location.search])
 
   return (
     <>
-      <PageContainer>
+      <SEO title="Search" />
+      <PageContainer id={"search-page"} activePath={location}>
         <PageContent>
           <h1>Search Site</h1>
           <SearchForm query={searchQuery} />
@@ -32,5 +32,3 @@ const SearchPage = ({ location }) => {
     </>
   )
 }
-
-export default SearchPage
